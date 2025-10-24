d681f4083a1bbeed89ab964bbb635e9a090b3c61
Giles Lemmon <51659929+zesty-lemon@users.noreply.github.com>
2025-10-24 12:07:21 -0400

    resample feature lengths & code clean up

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
 import os, glob
+from typing import Dict, Tuple
 
 import numpy as np
 from numpy.typing import NDArray
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from scipy.interpolate import interp1d
 
-def read_series(fp) -> NDArray[np.float32]:
-    # skip the header
-    return np.loadtxt(fp, skiprows=1, delimiter=',').astype(np.float32)
+# read csv into NDArray, skipping the first row
+def read_series(filepath) -> NDArray[np.float32]:
+    return np.loadtxt(filepath, skiprows=1, delimiter=',').astype(np.float32)
 
+
+# resample the raw data into a fixed length using interpolation
+def interpolate_signal(x_raw_data, target_len=50):
+    n = len(x_raw_data)
+    if n == target_len:
+        return x_raw_data
+    old_idx = np.linspace(0, 1, n)
+    new_idx = np.linspace(0, 1, target_len)
+    f = interp1d(old_idx, x_raw_data, kind='linear')
+    return f(new_idx)
+
+
+# read features in from file
+# split into features and labels
+# resize the features to be consistent length
+def read_and_resample_features(file_root: str, classes: Dict[str, int], target_len:int = 50) -> Tuple[np.ndarray, np.ndarray]:
+    X_features = []
+    y_labels = []
+
+    for label, val in classes.items():
+        for file in glob.glob(os.path.join(root, label, "*.csv")):
+            x_raw_data = read_series(file)
+            x_resampled = interpolate_signal(x_raw_data, target_len=target_len)
+            X_features.append(x_resampled)
+            y_labels.append(val)
+
+    X_features = np.array(X_features)
+    y_labels = np.array(y_labels)
+    return X_features, y_labels
+
+
+#---- Run Model -----
 root = "data/gonzalez_2017/data/"
 classes = {"potholes": 1, "regular_road": 0}
 
-X_features = []
-y_labels = []
-
-value_summary = []
-for label, val in classes.items():
-    min_len = 1000000000
-    max_len = 0
-    for f in glob.glob(os.path.join(root, label, "*.csv")):
-        x = read_series(f)
-        if len(x) > max_len:
-            max_len = len(x)
-        if len(x) < min_len:
-            min_len = len(x)
-        feats = [x.mean(), x.std(), x.min(), x.max(), np.ptp(x), np.mean(np.abs(np.diff(x)))]
-        X_features.append(feats)
-        y_labels.append(val)
-    print(label,"|  min: "+str(min_len)+" max: "+str(max_len))
-
-print(value_summary)
-X_features, y_labels = np.array(X_features), np.array(y_labels)
+# read features in from file and resample them
+X_features, y_labels = read_and_resample_features(root, classes, 50)
 
 # train test split
-Xtr, Xte, ytr, yte = train_test_split(X_features, y_labels, test_size=0.3, stratify=y_labels, random_state=42)
+Xtrain, Xtest, ytrain, ytest = train_test_split(X_features, y_labels, test_size=0.3, stratify=y_labels, random_state=42)
 
 # train model
 clf = RandomForestClassifier(n_estimators=200, random_state=42)
-clf.fit(Xtr, ytr)
+clf.fit(Xtrain, ytrain)
 
-# evaluate model
-ypred = clf.predict(Xte)
-print(classification_report(yte, ypred, target_names=["regular_road", "pothole"]))
+# evaluate model & print report
+ypred = clf.predict(Xtest)
+print(classification_report(ytest, ypred, target_names=["regular_road", "pothole"]))
